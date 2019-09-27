292e7d3f8a7df83053a7f42f5dc2cf659771d639
jmg1478 <50553817+jmg1478@users.noreply.github.com>
2019-09-27 15:51:00 -0700

    idk if this is going to work but if so that would be nice

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO Burgers (id, name, devoured, date)
+VALUES
+	(1, 'Hamburger', 15, '2019-9-27 02:21:12'),
+	(2, 'Classic Cheese Burger', 10, '2019-9-27 02:25:12'),
+	(3, 'Double Bacon Burger', 5, '2019-9-27 02:24:12'),
+	(4, "My Food's Food Burger", 3, '2019-9-27 02:23:12'),
+	(5, 'Chicken Burger', 2, '2019-9-27 02:22:12');
